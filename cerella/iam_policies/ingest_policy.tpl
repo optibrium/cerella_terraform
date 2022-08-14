1d245f9cb8a95aacea743296aa410218ab959dd4
Shashank Parab <shashank@optibrium.com>
2022-08-14 17:19:20 +0100

    Modules for IRSA and IAM Policy

---
+++ cerella/iam_policies/ingest_policy.tpl
@@ -0,0 +1,14 @@
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": [
+      "kms:Encrypt",
+      "kms:Decrypt"
+    ],
+    "Resource": [
+      "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab",
+      "arn:aws:kms:us-west-2:111122223333:key/0987dcba-09fe-87dc-65ba-ab0987654321"
+    ]
+  }
+}
--- cerella/main.tf
+++ cerella/main.tf
@@ -1,9 +1,14 @@
+module "ingest_iam_policy" {
+    source = "./modules/iam/iam-policy"
+    create_policy = true
+    description = "IAM Policy for ingest service."
+    policy = "${file("iam_policies/ingest_policy.tpl")}"
+}
 module "ingest_iam_role" {
-  source                        = "./modules/irsa"
-#   version                       = "~> v2.6.0"
+  source                        = "./modules/iam/iam-role-for-irsa"
   create_role                   = true
   role_name                     = "ingest"
   provider_url                  = replace(flatten(concat(aws_eks_cluster.environment[*].identity[*].oidc.0.issuer, [""]))[0], "https://", "")
-  role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.k8s_service_account_namespace}:${local.k8s_service_account_name}"]
+#   role_policy_arns              = [aws_iam_policy.ingest.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.k8s_service_account_namespace}:${var.k8s_service_account_name}"]
 }
