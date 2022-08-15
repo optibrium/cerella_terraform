e78be076d5bf61869eb164b4dc00dfe9e4031244
Shashank Parab <shashank@optibrium.com>
2022-08-15 06:41:05 +0100

    Adding KMS key

--- cerella/main.tf
+++ cerella/main.tf
@@ -1,3 +1,10 @@
+module ingest_kms_key {
+  source  = "./modules/kms"
+
+  alias_name              = "${var.cluster-name}-ingest-key"
+  description             = "Key to encrypt and decrypt secrets"
+
+}
 module "ingest_iam_policy" {
   source        = "./modules/iam/iam-policy"
   create_policy = true
@@ -9,7 +16,7 @@ module "ingest_iam_policy" {
 
       effect    = "Allow"
       actions   = ["kms:Encrypt", "kms:Decrypt"]
-      resources = ["arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab", "arn:aws:kms:us-west-2:111122223333:key/0987dcba-09fe-87dc-65ba-ab0987654321"]
+      resources = [module.ingest_kms_key.key_arn]
     }
   ]
 }
