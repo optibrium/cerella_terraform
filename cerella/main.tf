6fd740831ea0a969744bcd094cdc91f739c43d58
Tarek N. Elsamni <tarek@optibrium.com>
2023-02-28 03:31:49 +0000

    Add CloudWatch IAM policy/role

--- cerella/main.tf
+++ cerella/main.tf
@@ -66,3 +66,35 @@ module "ingest_irsa_iam_role" {
   role_policy_arns              = [module.ingest_iam_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.ingest_service_account_namespace}:${var.ingest_service_account_name}"]
 }
+
+module "cloudwatch_logs_iam_policy" {
+  source        = "./modules/iam/iam-policy"
+  create_policy = true
+  description   = "IAM Policy for CloudWatch Agent."
+  name_prefix   = "cloudwatch-agent-"
+  policy_statements = [
+    {
+      sid = "CloudWatchAccess"
+
+      effect = "Allow"
+      actions = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ]
+      resources = [
+        "arn:aws:logs:*:*:*"
+      ]
+    }
+  ]
+}
+
+module "cloudwatch_logs_iam_role" {
+  source                        = "./modules/iam/iam-role-for-irsa"
+  create_role                   = true
+  role_name                     = "cloudwatch-agent-${var.cluster-name}"
+  provider_url                  = local.oidc_provider_url
+  role_policy_arns              = [module.cloudwatch_logs_iam_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-for-fluent-bit"]
+}
